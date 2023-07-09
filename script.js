b857e50e77293a3b61d235f783238ed60898309d
Pedro Araujo <shikbane.developer@gmail.com>
2023-07-09 20:41:40 -0300

    Exclusão de console.logs

--- script.js
+++ script.js
@@ -102,9 +102,8 @@ const formatMovementDate = function (date, locale) {
   //Exbibindo as datas junto as transações
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
-
+    
   const daysPassed = calcDaysPassed(new Date(), date);
-  console.log(daysPassed);
 
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
